13b3d7c6c76bc4528cf85a4b8480057259331e78
M.Ali Yücesoylu <myucesoylu@gmail.com>
2022-12-15 17:26:08 +0300

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
+// import 'react-native-gesture-handler'
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your appp!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
 
---
+++ firabase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAxmncmYD7GCNXuMY7nEu_b1GNFMYpCnx4",
+  authDomain: "signal-clone-3295f.firebaseapp.com",
+  projectId: "signal-clone-3295f",
+  storageBucket: "signal-clone-3295f.appspot.com",
+  messagingSenderId: "591016292945",
+  appId: "1:591016292945:web:8fbce8df99b549455bbe70"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
